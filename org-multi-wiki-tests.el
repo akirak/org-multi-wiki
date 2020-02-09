65caa2961568236e5dc14ed923efb81ed604ece5
Akira Komamura <akira.komamura@gmail.com>
2020-02-10 05:25:34 +0900

    Add tests for file name escaping

---
+++ org-multi-wiki-tests.el
@@ -0,0 +1,30 @@
+(require 'buttercup)
+(require 'org-multi-wiki)
+
+(describe "Defaults"
+  (describe "org-multi-wiki-default-escape-file-name-fn"
+    (cl-flet ((escape-fn (heading) (org-multi-wiki-default-escape-file-name-fn heading)))
+
+      (it "does not camel case a single word"
+        (let ((result (escape-fn "hello123")))
+          (expect result :to-equal "hello123")))
+
+      (it "retain dots"
+        (let ((result (escape-fn "github.com")))
+          (expect result :to-equal "github.com")))
+
+      (it "retain CJK characters"
+        (let ((result (escape-fn "あいうえお雙拼輸入方案")))
+          (expect result :to-equal "あいうえお雙拼輸入方案")))
+
+      (it "join multiple words and convert to UpperCamelCase"
+        (let ((result (escape-fn "hello world")))
+          (expect result :to-equal "HelloWorld")))
+
+      (it "split words by hyphens and underscores as well"
+        (let ((result (escape-fn "ab-cd ef_gh")))
+          (expect result :to-equal "AbCdEfGh")))
+
+      (it "eliminates most symbols not specified in the above"
+        (let ((result (escape-fn "123@#!([]<\\|hello")))
+          (expect result :to-equal "123hello"))))))

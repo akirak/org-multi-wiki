f5ed07a4efa84dbbac003ca5d7dd21e1aa841eaa
Akira Komamura <akira.komamura@gmail.com>
2020-02-09 22:17:34 +0900

    Add CI for org-multi-wiki.el

---
+++ ci.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> {},
+  emacs ? (import (builtins.fetchTarball "https://github.com/purcell/nix-emacs-ci/archive/master.tar.gz")).emacs-25-1
+}:
+let
+  check-package = import (builtins.fetchTarball "https://github.com/akirak/emacs-package-checker/archive/v1.tar.gz");
+in
+{
+  org-multi-wiki = check-package {
+    inherit emacs pkgs;
+    name = "org-multi-wiki";
+    src = ./.;
+    targetFiles = ["org-multi-wiki.el"];
+    emacsPackages = epkgs: (with epkgs.melpaPackages; [
+      dash
+      s
+    ]);
+  };
+}

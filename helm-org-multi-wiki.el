e6f61cd4043eca0c4eaa99cb78b2ec456e814005
Akira Komamura <akira.komamura@gmail.com>
2020-02-09 16:01:58 +0900

    Initial commit

---
+++ helm-org-multi-wiki.el
@@ -0,0 +1,108 @@
+;;; helm-org-multi-wiki.el --- Helm interface to org-multi-wiki -*- lexical-binding: t -*-
+
+;; Copyright (C) 2020 Akira Komamura
+
+;; Author: Akira Komamura <akira.komamura@gmail.com>
+;; Version: 0.1
+;; Package-Requires: ((emacs "25.1") (org-multi-wiki "0.1") (org-ql "0.4") (helm-org-ql "0.4") (dash "2.12"))
+;; Keywords: org, outlines
+;; URL: https://github.com/akirak/org-multi-wiki
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This package provides a helm interface to org-multi-wiki.
+
+;;; Code:
+
+(require 'dash)
+(require 'org-multi-wiki)
+(require 'org-ql)
+(require 'helm-org-ql)
+
+(cl-defun helm-org-multi-wiki-select-ids (&key prompt action)
+  "Select directory IDs using helm.
+
+PROMPT and ACTION are passed to helm."
+  (helm :prompt (or prompt "Wikis")
+        :sources
+        (list (helm-build-sync-source "Wiki ID"
+                :candidates (mapcar (lambda (x)
+                                      (cons (format "%s (%s)"
+                                                    (car x)
+                                                    (nth 1 x))
+                                            (car x)))
+                                    org-multi-wiki-directories)
+                :action (or action (lambda (candidate) (or (helm-marked-candidates) candidate)))))))
+
+;;;###autoload
+(cl-defun helm-org-multi-wiki (&optional ids &key first)
+  "Visit an entry or create a new entry.
+
+IDS are are a list of directory ids.
+It can be a list of symbols or a symbol.
+
+When FIRST is given, it is the default target of entry creation."
+  (interactive)
+  ;; Based on the implementation of helm-org-ql.
+  (pcase current-prefix-arg
+    ('(4) (let ((current-prefix-arg nil))
+            (helm-org-multi-wiki-select-ids
+             :action
+             (list (cons "Run helm-org-mult-wiki on selected IDs"
+                         (lambda (_)
+                           (helm-org-multi-wiki (helm-marked-candidates))))))))
+    (_ (let* ((ids (cl-etypecase ids
+                     ;; Normalize IDs to make it a list of symbols.
+                     (null (list org-multi-wiki-current-directory-id))
+                     (list ids)
+                     (symbol (list ids))))
+              (boolean 'and)
+              (helm-input-idle-delay helm-org-ql-input-idle-delay)
+              (files (->> ids
+                          (mapcar #'org-multi-wiki-entry-files)
+                          (apply #'append))))
+         (helm :prompt (format "Query (boolean %s): " (-> boolean symbol-name upcase))
+               :sources
+               (list (helm-org-ql-source files
+                                         :name (format "Wiki (%s)"
+                                                       (mapconcat #'symbol-name ids ",")))
+                     (helm-build-dummy-source "New entry"
+                       :action
+                       (mapcar (lambda (id)
+                                 (cons (format "Create a new entry in %s%s"
+                                               id
+                                               (if (equal id first)
+                                                   " (current)"
+                                                 ""))
+                                       `(lambda (inp)
+                                          (org-multi-wiki-visit-entry inp :id ,id))))
+                               (if (and first (> (length ids) 1))
+                                   (cons first (-remove-item first ids))
+                                 ids)))))))))
+
+;;;###autoload
+(defun helm-org-multi-wiki-all ()
+  "Run `helm-org-multi-wiki' on all configured directories."
+  (interactive)
+  (helm-org-multi-wiki (mapcar #'car org-multi-wiki-directories)
+                       :first org-multi-wiki-current-directory-id))
+
+(provide 'helm-org-multi-wiki)
+;;; helm-org-multi-wiki.el ends here
---
+++ org-multi-wiki.el
@@ -0,0 +1,152 @@
+;;; org-multi-wiki.el --- Multiple wikis based on Org mode -*- lexical-binding: t -*-
+
+;; Copyright (C) 2020 Akira Komamura
+
+;; Author: Akira Komamura <akira.komamura@gmail.com>
+;; Version: 0.1
+;; Package-Requires: ((emacs "25.1") (dash "2.12"))
+;; Keywords: org outlines files
+;; URL: https://github.com/akirak/org-multi-wiki
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This library provides an infrastructure for multiple wikis on a
+;; single machine, based on org-mode.
+
+;; For configuration, see README.
+
+;; For practical use, see helm-org-multi-wiki.el.
+
+;;; Code:
+
+(require 'org)
+
+(defgroup org-multi-wiki nil
+  "Multiple wikis based on org-mode."
+  :group 'org)
+
+;;;;  Custom variables
+(defcustom org-multi-wiki-directories
+  `((default ,org-directory))
+  "List of directories containing wiki entries.
+
+Each entry in this variable should be a list containing the
+following items, in that order:
+
+- A symbol to uniquely identify the directory.
+- A file path to the directory containing Org files."
+  :type '(repeat (list (symbol :tag "ID")
+                       (directory :tag "Directory")))
+  :group 'org-multi-wiki)
+
+(defcustom org-multi-wiki-default-directory-id (caar org-multi-wiki-directories)
+  "ID of the default wiki directory.
+
+This should be the first element of one of the entries in
+`org-multi-wiki-directories'."
+  :type 'symbol
+  :group 'org-multi-wiki)
+
+(defcustom org-multi-wiki-escape-file-name-fn #'org-multi-wiki-default-escape-file-name-fn
+  "Function used to generated an escaped file name from a heading."
+  :type 'function
+  :group 'org-multi-wiki)
+
+(defcustom org-multi-wiki-entry-template-fn #'org-multi-wiki-default-entry-template-fn
+  "Function to create an initial Org entry from a heading."
+  :type 'function
+  :group 'org-multi-wiki)
+
+(defcustom org-multi-wiki-find-file-fn #'find-file-other-window
+  "Function used to visit an Org file."
+  :type 'function
+  :group 'org-multi-wiki)
+
+(defcustom org-multi-wiki-ignore-missing-directories nil
+  "When non-nil, return an empty result from `org-multi-wiki-entry-files' when the directory does not exist."
+  :type 'boolean
+  :group 'org-multi-wiki)
+
+;;;; Other variables
+(defvar org-multi-wiki-current-directory-id nil)
+
+;;;; Default functions
+(defun org-multi-wiki-default-escape-file-name-fn (heading)
+  "Escape HEADING suitable for use in file name."
+  (cl-labels ((filename-escape
+               (str)
+               (s-replace-regexp (rx (not (any space alnum "-"))) "" str)))
+    (let ((words (split-string heading " ")))
+      (if (= 1 (length words))
+          (filename-escape (car words))
+        (-> words
+            (-map #'filename-escape)
+            (-map #'s-upper-camel-case)
+            (string-join))))))
+
+(defun org-multi-wiki-default-entry-template-fn (heading)
+  "Generate an Org entry from HEADING."
+  (concat "* " heading "\n"))
+
+;;;; File and directory infrastructure
+(defun org-multi-wiki-directory (&optional id)
+  "Get the directory of a wiki ID."
+  (let ((id (or id org-multi-wiki-current-directory-id)))
+    (or (car-safe (alist-get id org-multi-wiki-directories))
+        (error "No entry exists for %s in org-multi-wiki-directories" id))))
+
+(defun org-multi-wiki-select-directory-id (&optional prompt)
+  "Select a wiki id using `completing-read', with an optional PROMPT."
+  (intern (completing-read (or prompt "Wiki: ")
+                           (mapcar #'car org-multi-wiki-directories))))
+
+;;;###autoload
+(defun org-multi-wiki-entry-files (&optional id)
+  "Get a list of Org files in the directory with ID."
+  (directory-files (org-multi-wiki-directory id) t org-agenda-file-regexp))
+
+;;;; Commands
+
+;;;###autoload
+(defun org-multi-wiki-switch (id)
+  "Set the current wiki to ID."
+  (interactive (list (org-multi-wiki-select-directory-id)))
+  (when-let ((dir (org-multi-wiki-directory id)))
+    (setq org-multi-wiki-current-directory-id id)
+    (message "Set the current wiki to \"%s\" (%s)" id dir)))
+
+;;;###autoload
+(cl-defun org-multi-wiki-visit-entry (heading &key id)
+  "Visit an Org file for HEADING in the directory with ID."
+  (let* ((dir (org-multi-wiki-directory id))
+         (filename (concat (funcall org-multi-wiki-escape-file-name-fn heading) ".org"))
+         (fpath (expand-file-name filename dir)))
+    (unless (and dir (file-directory-p dir))
+      (user-error "Wiki directory is nil or missing: %s" dir))
+    (unless (file-exists-p fpath)
+      ;; Set default-directory to allow directory-specific templates
+      (let ((default-directory dir))
+        (with-temp-buffer
+          (insert (org-multi-wiki-template heading))
+          (write-file fpath))))
+    (funcall org-multi-wiki-find-file-fn fpath)))
+
+(provide 'org-multi-wiki)
+;;; org-multi-wiki.el ends here

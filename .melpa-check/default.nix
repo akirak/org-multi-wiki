9ddd1727fc546853683abb2c60091b3c6e40494f
Akira Komamura <akira.komamura@gmail.com>
2020-04-12 12:23:41 +0900

    Meta: Don't set the default Emacs in the Nix file; Update the repository name

--- .melpa-check/default.nix
+++ .melpa-check/default.nix
@@ -1,9 +1,9 @@
 { pkgs ? import <nixpkgs> {},
-  emacs ? import (builtins.fetchTarball "https://github.com/purcell/nix-emacs-ci/archive/master.tar.gz").emacs-26-1,
+  emacs,
   srcDir ? ../.,
   testDir ? ../.,
   packageFile ? ".melpa-check/packages.dhall"
 }:
-import (builtins.fetchTarball "https://github.com/akirak/emacs-package-checker/archive/v3.tar.gz") {
+import (builtins.fetchTarball "https://github.com/akirak/melpa-check/archive/v3.tar.gz") {
   inherit pkgs emacs packageFile srcDir testDir;
 }
